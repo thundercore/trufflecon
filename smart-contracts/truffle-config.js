a917f81f73e1efd66870d6b6543e6bed66a57f58
Julien Tregoat <julien.tregoat@thundertoken.com>
2019-07-25 13:56:12 -0700

    Add stablecoin support for DoubleOrNothing smart contract.

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -61,7 +61,17 @@ module.exports = {
         )
       },
       network_id: 108
+    },
+    thunderTest: {
+      provider: function() {
+        return new HDWallet(
+          '',
+          'https://testnet-rpc.thundercore.com'
+        )
+      },
+      network_id: 18
     }
+
 
     // Another network with more advanced options...
     // advanced: {
